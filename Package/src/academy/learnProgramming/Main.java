d5bdfcfb29a25484835520d3751948a74a92f463
Derek Leung <hkz88i00123@gmail.com>
2020-04-01 22:03:20 +0800

    scope

---
+++ Package/src/academy/learnProgramming/Main.java
@@ -0,0 +1,9 @@
+package academy.learnProgramming;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ WinDemo/src/com/example/awtSample/Main.java
@@ -0,0 +1,9 @@
+package com.example.awtSample;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    myWindow myWindow = new myWindow("Complete Java");
+	    myWindow.setVisible(true);
+    }
+}
